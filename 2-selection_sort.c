1dd13991d192bfd138c227da157160ddbb370581
Stella <stella.me90@gmail.com>
2022-10-09 01:08:56 -0700

    task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -30,4 +30,16 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
 
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
